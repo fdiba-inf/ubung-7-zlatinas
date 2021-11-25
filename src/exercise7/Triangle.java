94da79bd268683c03f373ed23a86ef08ad3d4a5b
zlatinas <>
2021-11-25 10:41:05 +0000

    z

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -12,11 +12,22 @@ public class Triangle {
         for (int line = 1; line <= n; line++) {
             printLine(1, line);
         }
+        for ( int line =n - 1; line > 0; line --) {
+          printLine(1, line);
+        }
 
         // Print the bottom part of the triangle that is under the longest line
     }
 
     public static void printLine(int start, int end) {
-        // Implement method
+      do {
+        if (start < end) {
+          System.out.print(start + " ");
+
+        } else {
+          System.out.println(start);
+        }
+        start++;
+      } while (start <= end);
     }
 }
